10029963c75e5c1c32114c2336b307b0ab00df5b
Piotr Borczyk <howorang@gmail.com>
2017-10-23 22:39:19 +0200

    initial commit

---
+++ app/src/main/java/edu/groups/app/MainActivity.java
@@ -0,0 +1,18 @@
+package edu.groups.app;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.widget.Button;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.stream.StreamSupport;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
